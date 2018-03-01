a354fab2dace3e18732c96ee6c1e38bb87a3df7f
Diplow <u.boillot@gmail.com>
2018-03-01 19:37:46 +0100

    gogo

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from reader import readSubject
 
-subject = "input/a_example.in"
-
-STEP_COUNT, BONUS, rides, cars = readSubject(subject)
-
-step = 0
-while step < STEP_COUNT:
-    for car in cars:
-        if car.isAvailable(step):
-            car.findRide(rides)
+for f in ["subject/a_example.in", "subject/b_should_be_easy.in", "subject/c_no_hurry.in", "subject/d_metropolis.in", "subject/e_high_bonus.in"]:
+    STEP_COUNT, BONUS, rides, cars = readSubject(f)
+    step = 0
+    while step < STEP_COUNT:
+        for car in cars:
+            car.update()
+            if car.isAvailable(step):
+                car.findRide(rides)
